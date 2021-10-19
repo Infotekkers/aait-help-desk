104854862c2de61acf8467f2012ef13bae7e441e
nati-m <it.natnael.mekonnen@gmail.com>
2021-10-19 17:53:16 +0300

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route, Link, Router } from "react-router-dom";
 import Curriculum from "./pages/Curriculum";
 import "./App.css";
 
-const App = () => {
-  return <Curriculum />;
-};
+const App = () => (
+  <BrowserRouter>
+    // TODO: NavBar
+  <Switch>
+<Route path="/">
+  <h1>TODO: Landing Page</h1>
+</Route>
+<Route path="/curriculum">
+  <Curriculum />
+</Route>
+</Switch>
+</BrowserRouter>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
